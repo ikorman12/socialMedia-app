f4e83a27daeb7a730d7ad55ff8b0e481b1eac6b7
Igor Korman <ikorman12@gmail.com>
2022-12-03 06:02:36 -0500

    finalized changes to username model

--- models/Username.js
+++ models/Username.js
@@ -28,20 +28,14 @@ const usernameSchema = new Schema(
 );
 
 // Create a virtual property `fullName` that gets and sets the user's full name
-userSchema
-  .virtual("fullName")
+usernameSchema
+  .virtual("profile")
   // Getter
   .get(function () {
-    return `${this.first} ${this.last}`;
+    return `${this.username}`;
   })
-  // Setter to set the first and last name
-  .set(function (v) {
-    const first = v.split(" ")[0];
-    const last = v.split(" ")[1];
-    this.set({ first, last });
-  });
 
 // Initialize our User model
-const User = model("user", userSchema);
+const Username = model("username", usernameSchema);
 
-module.exports = User;
+module.exports = Username;
