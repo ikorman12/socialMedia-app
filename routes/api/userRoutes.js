6adbdb0584a02fd2f5df73d7faa064aca813ddb0
Igor Korman <ikorman12@gmail.com>
2022-12-04 20:52:31 -0500

    added user routes, added date utility

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,12 +3,34 @@ const {
   getUsers,
   getSingleUser,
   createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
 } = require('../../controllers/userController');
 
 // /api/users
-router.route('/').get(getUsers).post(createUser);
+router
+.route('/')
+//get all users
+.get(getUsers)
+//create a user
+.post(createUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getSingleUser);
+router
+.route('/:userId')
+//GET single User
+.get(getSingleUser)
+//DELETE single user
+.delete(deleteUser)
+//PUT/update user
+.put(updateUser);
+
+router.route('/:userId/friends/friendId')
+//add a friend
+.post(addFriend)
+//delete friend
+.delete(removeFriend)
 
 module.exports = router;
